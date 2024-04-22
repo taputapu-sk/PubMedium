aaff11aeb0a63d99a0bb97f7ef74f9a96fa06f93
Stanislav <don@don-arborio.chimgan.net>
2024-04-22 19:07:52 +0200

    Code\pubmed_corpus_creator.py

--- Code/pubmed_corpus_creator.py
+++ Code/pubmed_corpus_creator.py
@@ -5,7 +5,14 @@ from numpy.random import shuffle
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 
-PMC_BASE_URL = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
+NCBI_BASE_URL = "https://www.ncbi.nlm.nih.gov"
+PMC_BASE_ARTICLES_URL = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    }
 
 class PubMedCorpusCreator(PubMedFetcher):
     """
@@ -25,28 +32,22 @@ class PubMedCorpusCreator(PubMedFetcher):
             if "PMC" in publication.article_ids:
                 pmc_url = self._get_pmc_url(publication.article_ids["PMC"])
                 pdf_link = self._get_pdf_link(pmc_url)
+                pass
                 # print(publication.article_ids["PMC"])
 
 
     def _get_pmc_url(self, pmc_id: str) -> str:
-        return f"{PMC_BASE_URL}{pmc_id}/"
+        return f"{PMC_BASE_ARTICLES_URL}{pmc_id}/"
 
     def _get_pdf_link(self, pmc_url: str) -> str:
-
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9",
-            }
-
-        request = requests.get(pmc_url, allow_redirects=True, headers=headers)
+        request = requests.get(pmc_url, allow_redirects=True, headers=HEADERS)
         response = request.text
 
         soup = BeautifulSoup(response)
 
-        x_link = soup.find("li", {"class": "pdf-link"})
-        pass
+        x_link = soup.find("a", {"class": "int-view"})
+        link = x_link.attrs["href"]
+        return f"{NCBI_BASE_URL}{link}"
 
 if __name__ == '__main__':
     fetcher = PubMedCorpusCreator()
