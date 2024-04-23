17364e8e13a1d822d735455ab5b97770e4725bf9
Stanislav <don@don-arborio.chimgan.net>
2024-04-23 05:31:47 +0200

    Code\pubmed_corpus_creator.py : issues, comments

--- Code/pubmed_corpus_creator.py
+++ Code/pubmed_corpus_creator.py
@@ -1,16 +1,19 @@
 import os.path
 import shutil
 import subprocess
-
 import requests
-from pathlib import Path
-from Code.pubmed_fetcher import PubMedFetcher
 from numpy.random import shuffle
 from bs4 import BeautifulSoup
-import xml.etree.ElementTree as ET
+from pubmed_fetcher import PubMedFetcher
 
+# Base URL to fetch PDFs from:
 NCBI_BASE_URL = "https://www.ncbi.nlm.nih.gov"
+
+# Base URL to get PMC article pages:
 PMC_BASE_ARTICLES_URL = "https://www.ncbi.nlm.nih.gov/pmc/articles/"
+
+# Headers for HTTP request necessary for the PMC server to accept your request
+# (without it, it would think that you are a bot, and refuse to response).
 HEADERS_PDF_LINK = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
@@ -18,29 +21,45 @@ HEADERS_PDF_LINK = {
     "Accept-Language": "en-US,en;q=0.9",
 }
 
+# Headers for HTTP request to download a PDF file.
 HEADERS_PDF = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
     "Accept": "application/pdf",
 }
 
+# Name for the temporary PDF file (presumed you are on Windows and C:\Temp exists).
 TEMP_PDF = r"C:\Temp\temp_pubmed.pdf"
 TEMP_TXT = r"C:\Temp\temp_pubmed.txt"
 
 
 class PubMedCorpusCreator(PubMedFetcher):
     """
-    Vehicula, suspendisse massa gravida. Turpis velit commodo lobortis tristique hendrerit, scelerisque nec iaculis.
-    Vulputate pellentesque rutrum lorem mattis litora tempor. Lacus curae; posuere, habitasse interdum.
-    Feugiat torquent etiam consequat ornare nunc gravida feugiat quam purus scelerisque bibendum.
-    Sapien adipiscing turpis id auctor in ligula parturient. Nisl nam donec congue curae; fusce
-    lacinia duis vitae.
+    Creator of topic text corpora from PubMed publications.
     """
+    def create_corpus(self, size: int, topics: list[str], output_folder: str, corpus_name: str = ""):
+        """
+        Creates a text file corpus for a list of topics.
+        :param size: The maximum size of the corpus to create.
+        :param topics: List of topics to create the corpus from (combined by AND).
+        :param output_folder: The name of the folder in which to create the corpus.
+                              If the folder does not exist, it will be created.
+        :param corpus_name: The name of the subfolder of output_folder in which to write the text files.
+                            If left empty, the corpus name will be created using the topics.
+        """
+        if len(topics) == 0:
+            print("No topics defined. Canceling.")
+            return
 
-    def create_corpus(self, size: int, topics: list[str], output_folder: str, corpus_name: str):
+        PubMedFetcher.print_intermediate_results = False
         ids = self._extract_ids_by_topics(topics)
         shuffle(ids)
 
+
+        print(f"Found {len(ids)} publications for the topics.")
         publications = self._extract_publications(ids)
+
+        if len(corpus_name) < 2:
+            corpus_name = "_".join(topics)
 
         if not os.path.exists(output_folder):
             os.mkdir(output_folder)
@@ -50,6 +69,7 @@ class PubMedCorpusCreator(PubMedFetcher):
         if not os.path.exists(corpus_folder):
             os.mkdir(corpus_folder)
 
+        count = 0
         for publication in publications:
             if "PMC" in publication.article_ids:
                 pmc_id = publication.article_ids["PMC"]
@@ -57,16 +77,22 @@ class PubMedCorpusCreator(PubMedFetcher):
                 pdf_link = self._get_pdf_link(pmc_url)
 
                 if not self._download_pdf(pdf_link):
+                    print("PDF link not found. Skipping.")
                     continue
 
                 if not self._convert_to_text():
+                    print("Conversion from PDF to text failed. Skipping.")
                     continue
 
                 file_name = f"{corpus_folder}/{pmc_id}.txt"
                 shutil.copyfile(TEMP_TXT, file_name)
 
-                print(f"Copied {file_name}")
+                count += 1
+                print(f"Copied {file_name} ({count} of {len(publications)})")
 
+                if count >= size:
+                    print(f"*** All publications processed. Created {count} text files.")
+                    break
 
     def _get_pmc_url(self, pmc_id: str) -> str:
         return f"{PMC_BASE_ARTICLES_URL}{pmc_id}/"
@@ -75,7 +101,7 @@ class PubMedCorpusCreator(PubMedFetcher):
         request = requests.get(pmc_url, allow_redirects=True, headers=HEADERS_PDF_LINK)
         response = request.text
 
-        soup = BeautifulSoup(response, parser="html")
+        soup = BeautifulSoup(response, "html.parser")
 
         x_link = soup.find("a", {"class": "int-view"})
         link = x_link.attrs["href"]
@@ -97,7 +123,8 @@ class PubMedCorpusCreator(PubMedFetcher):
     def _convert_to_text(self) -> bool:
         try:
             #subprocess.run(["pdftotext", "-layout", "-nodiag", TEMP_PDF])
-            subprocess.run(["pdftotext", TEMP_PDF])
+            # Add stderr=subprocess.DEVNULL to supress annoying warnings from MikTeX.
+            subprocess.run(["pdftotext", "-q", TEMP_PDF], stderr=subprocess.DEVNULL)
             return True
         except:
             return False
@@ -105,4 +132,4 @@ class PubMedCorpusCreator(PubMedFetcher):
 if __name__ == '__main__':
     fetcher = PubMedCorpusCreator()
 
-    fetcher.create_corpus(100, ["dicom", "mri", "prostate"], "C:/Temp", "dicom_mri")
+    fetcher.create_corpus(5, ["dicom", "mri", "prostate"], "C:/Temp")
